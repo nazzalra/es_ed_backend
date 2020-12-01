9c4c457f77cd77fbc8a50d4f66682f21d5af7845
nazzal ramzan <naramzano@gmail.com>
2020-12-02 05:08:30 +0700

    refactor GejalaController

--- app/Http/Controllers/GejalaController.php
+++ app/Http/Controllers/GejalaController.php
@@ -16,37 +16,26 @@ class GejalaController extends Controller{
         return $json;
     }
     public function identifikasi(Request $request){
-        $daftar_aturan = $this->getAturanGejala($request->gejala);
-        $m3 = array();
+        $daftar_aturan = $this->dapatkanAturan($request->gejala);
+        $m3 = [];
         while(!empty($daftar_aturan)){
-            $m1 = $this->setBPA($e = array_shift($daftar_aturan));
-            $m2 = empty($m3)?$this->setBPA($e = array_shift($daftar_aturan)):$this->setBPAFrom($m3);
-            $m3 = array();
-            for ($i = 0; $i < count($m2); $i++) {
-                for ($j = 0; $j < 2; $j++) {
-                        $key = $this->getKey($m1[$j],$m2[$i]);
-                        if (!isset($m3[$key])) { 
-                            $m3[$key] = $m1[$j]->nilai_keyakinan * $m2[$i]->nilai_keyakinan;
-                        } else {
-                            $m3[$key] += $m1[$j]->nilai_keyakinan * $m2[$i]->nilai_keyakinan;
-                        }
-                }
-            }
-            $m3 = $this->hitungAturanDempster($m3);
+            $m1 = $this->tentukanBPA(array_shift($daftar_aturan));
+            $m2 = empty($m3)?$this->tentukanBPA(array_shift($daftar_aturan)):$this->tentukanBPAdari($m3);
+            $m3 = $this->hitungAturanDempster($m1,$m2);
         }
         return response()->json(
-            $this->getKesimpulan($m3)
+            $this->dapatkanKesimpulan($m3)
         );
     }
-    protected function getAturanGejala($gejala){
+    protected function dapatkanAturan($gejala){
         return json_decode(Aturan::gejala($gejala));
     }
-    protected function setBPA($e){
+    protected function tentukanBPA($e){
         $m[0] = $e;
-        $m[1] = Gejala::createFod($m[0]->nilai_keyakinan);
+        $m[1] = Gejala::tentukanFod($m[0]->nilai_keyakinan);
         return $m;
     }
-    protected function setBPAFrom($m3){
+    protected function tentukanBPAdari($m3){
         foreach ($m3 as $key => $val) { // konvert dari array assosiative ke objek
                 $m = new stdClass;
                 $m->gangguan = $key;
@@ -55,7 +44,7 @@ class GejalaController extends Controller{
         }
         return $m2;
     }
-    protected function getKey($m1_j,$m2_i){
+    protected function dapatkanHimpunanGangguan($m1_j,$m2_i){
         $x = explode(',', $m1_j->gangguan);
         $y = explode(',', $m2_i->gangguan);
         sort($x);
@@ -64,25 +53,38 @@ class GejalaController extends Controller{
         // buat array assosiative
         return empty($xy)?"himpunan_kosong":implode(',',$xy);
     }
-    protected function hitungAturanDempster($m3){
-        foreach ($m3 as $key => $val) {
-            if ($key != "himpunan_kosong") {
-                $m3[$key] = $val / (1 - (isset($m3["himpunan_kosong"]) ? $m3["himpunan_kosong"] : 0));
+    protected function hitungAturanDempster($m1,$m2){
+        $m3 = [];
+        for ($i = 0; $i < count($m2); $i++) {
+            for ($j = 0; $j < 2; $j++) {
+                    $himpunan_gangguan = $this->dapatkanHimpunanGangguan($m1[$j],$m2[$i]);
+                    if (!isset($m3[$himpunan_gangguan])) { 
+                        $m3[$himpunan_gangguan] = $m1[$j]->nilai_keyakinan * $m2[$i]->nilai_keyakinan;
+                    } else {
+                        $m3[$himpunan_gangguan] += $m1[$j]->nilai_keyakinan * $m2[$i]->nilai_keyakinan;
+                    }
+            }
+        }
+        return $this->tanpaEvidentialConflict($m3);
+    }
+    protected function dapatkanDataGangguan($ids){
+        return Gangguan::whereIn('id',explode(",",$ids))->get();
+    }
+    protected function dapatkanKesimpulan($m3){
+        array_pop($m3);
+        arsort($m3);
+        return [
+            'gangguan' => $this->dapatkanDataGangguan(array_key_first($m3)),
+            'nilai_keyakinan' => array_shift($m3)
+        ];
+    }
+    protected function tanpaEvidentialConflict($m3){
+        foreach ($m3 as $himpunan_gangguan => $nilai) {
+            if ($himpunan_gangguan != "himpunan_kosong") {
+                $m3[$himpunan_gangguan] = $nilai / (1 - (isset($m3["himpunan_kosong"]) ? $m3["himpunan_kosong"] : 0));
             }
         }
         unset($m3["himpunan_kosong"]);
         return $m3;
-    }
-    protected function getGangguan($ids){
-        return Gangguan::whereIn('id',explode(",",$ids))->get();
-    }
-    protected function getKesimpulan($m3){
-        unset($m3[implode(',',Aturan::fod())]);
-        arsort($m3);
-        $keys = array_keys($m3);
-        return [
-            'gangguan' => $this->getGangguan($keys[0]),
-            'nilai_keyakinan' => $m3[$keys[0]]
-        ];
     }
 }
--- app/Models/Gejala.php
+++ app/Models/Gejala.php
@@ -12,7 +12,7 @@ class Gejala extends Model {
     public function aturan(){
         return $this->belongsToMany(Gangguan::class,'aturan','id_gejala','id_gangguan')->withPivot('nilai_keyakinan');
     }
-    public static function createFod($nilai_gejala){
+    public static function tentukanFod($nilai_gejala){
             $fodObj = new stdClass;
             $fodObj->gangguan = implode(',', Aturan::fod());
             $fodObj->nilai_keyakinan = 1 - $nilai_gejala;
